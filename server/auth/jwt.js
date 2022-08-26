6e03a0e253739b37765824043367e871e4cea34b
jyeh20 <jmyeh51@gmail.com>
2022-08-26 15:38:38 -0700

    Updates to auth file

--- server/auth/jwt.js
+++ server/auth/jwt.js
@@ -2,15 +2,18 @@ import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
 import { User } from "../models";
 
+/* istanbul ignore next */
 const nodeEnv = process.env.NODE_ENV || "development";
 
 switch (nodeEnv) {
+  /* istanbul ignore next */
   case "production":
     dotenv.config({ path: "../config/config.prod.env" });
     break;
   case "test":
     dotenv.config({ path: "../config/config.test.env" });
     break;
+  /* istanbul ignore next */
   default:
     dotenv.config({ path: "../config/config.dev.env" });
 }
@@ -19,11 +22,12 @@ const generateToken = (user, expiresIn = "1hr") => {
   try {
     const userObj = new User(user);
     return jwt.sign(
-      { id: userObj.id, username: userObj.username, admin: userObj?.admin },
+      { id: userObj?.id, username: userObj?.username, admin: userObj?.admin },
       process.env.JWT_SECRET,
       { expiresIn }
     );
   } catch (error) {
+    /* istanbul ignore next */
     throw error;
   }
 };
