43fcc5d535ed29148e2d2a1f0b2aa9c7f2dfc613
jyeh20 <jmyeh51@gmail.com>
2022-08-26 15:39:00 -0700

    Updates to models

--- server/models/Commission.js
+++ server/models/Commission.js
@@ -1,14 +1,45 @@
 class Commission {
-  constructor(input) {
-    this.creator_id = input.creator_id;
-    this.name = input.name;
-    this.description = input.description;
-    this.freq_week = input.freq_week;
-    this.freq_month = input.freq_month;
-    this.freq_year = input.freq_year;
-    this.difficulty = input.difficulty;
-    this.num_times_completed = input.num_times_completed;
-    this.completed = input.completed;
+  constructor(props) {
+    this.id = props.id;
+    this.creator_id = props.creator_id;
+    this.name = props.name;
+    this.description = props.description;
+    this.freq_type = props.freq_type;
+    this.freq = props.freq || 1;
+    this.difficulty = props.difficulty || 1;
+    this.num_times_completed = props.num_times_completed || 0;
+    this.completed = props.completed || 0;
+
+    if (!this.creator_id || !this.name || !this.freq_type) {
+      const e = new Error("Missing required fields");
+      e.code = 403;
+      throw e;
+    }
+
+    if (
+      this.freq < 0 ||
+      this.difficulty < 0 ||
+      this.num_times_completed < 0 ||
+      this.completed < 0
+    ) {
+      const e = new Error("Fields cannot be negative");
+      e.code = 403;
+      throw e;
+    }
+  }
+
+  getCommission() {
+    return {
+      id: this.id,
+      creator_id: this.creator_id,
+      name: this.name,
+      description: this.description,
+      freq_type: this.freq_type,
+      freq: this.freq,
+      difficulty: this.difficulty,
+      num_times_completed: this.num_times_completed,
+      completed: this.completed,
+    };
   }
 
   getCommissionAsArray() {
@@ -16,12 +47,11 @@ class Commission {
       this.creator_id,
       this.name,
       this.description,
-      this.freq_week || 0,
-      this.freq_month || 0,
-      this.freq_year || 0,
-      this.difficulty || 1,
-      this.num_times_completed || 0,
-      this.completed || 0,
+      this.freq_type,
+      this.freq,
+      this.difficulty,
+      this.num_times_completed,
+      this.completed,
     ];
   }
 }
--- server/models/User.js
+++ server/models/User.js
@@ -1,24 +1,22 @@
-import e from "express";
-
 class User {
-  constructor(input) {
-    this.id = input.id;
-    this.first_name = input.first_name;
-    this.last_name = input.last_name;
-    this.username = input.username;
-    this.birthday = input.birthday || null;
-    this.email = input.email;
-    this.password = input.password;
-    this.daily_reward = input.daily_reward || 5;
-    this.weekly_reward = input.weekly_reward || 5;
-    this.monthly_reward = input.monthly_reward || 5;
-    this.yearly_reward = input.yearly_reward || 5;
-    this.max_commissions_day = input.max_commissions_day || 5;
-    this.max_commissions_week = input.max_commissions_week || 5;
-    this.max_commissions_month = input.max_commissions_month || 3;
-    this.max_commissions_year = input.max_commissions_year || 3;
-    this.points = input.points || 0;
-    this.admin = input.admin || false;
+  constructor(props) {
+    this.id = props.id;
+    this.first_name = props.first_name;
+    this.last_name = props.last_name;
+    this.username = props.username;
+    this.birthday = props.birthday || null;
+    this.email = props.email;
+    this.password = props.password;
+    this.daily_reward = props.daily_reward || 5;
+    this.weekly_reward = props.weekly_reward || 5;
+    this.monthly_reward = props.monthly_reward || 5;
+    this.yearly_reward = props.yearly_reward || 5;
+    this.max_commissions_day = props.max_commissions_day || 5;
+    this.max_commissions_week = props.max_commissions_week || 5;
+    this.max_commissions_month = props.max_commissions_month || 3;
+    this.max_commissions_year = props.max_commissions_year || 3;
+    this.points = props.points || 0;
+    this.admin = props.admin || false;
 
     if (
       !this.first_name ||
