dc8502db5bf8eeac43934d66e9281efb1df8d7a7
jyeh20 <jmyeh51@gmail.com>
2022-08-26 15:38:52 -0700

    Added 'Item' model

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,6 @@
 import User from "./User.js";
 import Commission from "./Commission.js";
 import Goal from "./Goal.js";
+import Item from "./Item.js";
 
-export { User, Commission, Goal };
+export { User, Commission, Goal, Item };
