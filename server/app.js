10e39786fff2c8a87310aebfaf174f297b118de3
jyeh20 <jmyeh51@gmail.com>
2022-09-09 13:44:38 -0700

    Patched CORS errors

--- server/app.js
+++ server/app.js
@@ -1,12 +1,24 @@
 import express from "express";
 import bodyParser from "body-parser";
 import * as routes from "./routes/index.js";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use((_, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:19006");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 app.use(routes.users);
 app.use(routes.commissions);
 app.use(routes.goals);
