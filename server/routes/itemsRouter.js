f2036ea5f5d0ade6a5f2b001be3fb452195e36a1
jyeh20 <jmyeh51@gmail.com>
2022-09-09 13:45:43 -0700

    Patched routes

--- server/routes/itemsRouter.js
+++ server/routes/itemsRouter.js
@@ -3,11 +3,11 @@ import * as ctrl from "../controllers/items.js";
 
 const router = express.Router();
 
-router.get("/userItems", ctrl.getItems);
-router.get("/userItem/id/:id", ctrl.getItemById);
-router.get("/userItem/name/:name", ctrl.getItemByName);
-router.post("/userItem", ctrl.createItem);
-router.put("/userItem", ctrl.updateItem);
-router.delete("/userItem", ctrl.deleteItem);
+router.get("/items", ctrl.getItems);
+router.get("/items/id/:id", ctrl.getItemById);
+router.get("/items/name/:name", ctrl.getItemByName);
+router.post("/items", ctrl.createItem);
+router.put("/items", ctrl.updateItem);
+router.delete("/items", ctrl.deleteItem);
 
 export default router;
--- server/routes/usersRouter.js
+++ server/routes/usersRouter.js
@@ -3,8 +3,7 @@ import * as ctrl from "../controllers/users.js";
 
 const router = new Router();
 
-router.get("/users/username", ctrl.getSelfByUsername);
-router.get("/users/id", ctrl.getSelfById);
+router.get("/users", ctrl.getUserWithToken);
 router.post("/users", ctrl.createUser);
 router.post("/users/login/email", ctrl.getUserWithEmailLogin);
 router.post("/users/login/username", ctrl.getUserWithUsernameLogin);
